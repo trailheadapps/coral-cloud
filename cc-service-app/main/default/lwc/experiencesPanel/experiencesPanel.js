f31de60a131d2e29e5a45268e0ba656cbb2da436
Philippe Ozil <pozil@salesforce.com>
2024-11-06 06:58:10 +0100

    feat: LWC best practices and various fixes on Experiences site components (#23)
    * feat: LWC best practices and various fixes
    
    * fix: remove special char from sample data

--- cc-service-app/main/default/lwc/experiencesPanel/experiencesPanel.js
+++ cc-service-app/main/default/lwc/experiencesPanel/experiencesPanel.js
@@ -1,50 +1,62 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import EXPERIENCE_TYPE_FIELD from '@salesforce/schema/Experience__c.Type__c';
 import EXPERIENCE_SELECTED_MESSAGE from '@salesforce/messageChannel/ExperienceSelected__c';
 import getExperiences from '@salesforce/apex/ExperienceController.getExperiences';
 
 export default class ExperiencePanel extends LightningElement {
-    pageNumber = 1;
+    types = [];
+    type;
     pageSize;
-    totalItemCount = 0;
-    types = [
-        { label: 'Adventure Activities', value: 'Adventure Activities' },
-        { label: 'Beaches & Snorkeling', value: 'Beaches & Snorkeling' },
-        {
-            label: 'Cultural Tours & Workshops',
-            value: 'Cultural Tours & Workshops'
-        },
-        { label: 'Dining Experiences', value: 'Dining Experiences' },
-        {
-            label: "Family & Kids' Activities",
-            value: "Family & Kids' Activities"
-        },
-        { label: 'Fitness & Exercise', value: 'Fitness & Exercise' },
-        { label: 'Golf', value: 'Golf' },
-        { label: 'Nature & Eco Tours', value: 'Nature & Eco Tours' },
-        {
-            label: 'Nightlife & Entertainment',
-            value: 'Nightlife & Entertainment'
-        },
-        {
-            label: 'Relaxation & Quiet Zones',
-            value: 'Relaxation & Quiet Zones'
-        },
-        { label: 'Spa & Wellness', value: 'Spa & Wellness' },
-        { label: 'Swimming Pools', value: 'Swimming Pools' },
-        { label: 'Tennis & Pickleball', value: 'Tennis & Pickleball' },
-        { label: 'Water Sports', value: 'Water Sports' }
-    ];
-    type = 'Adventure Activities';
-    recordTypeId;
+    totalItemCount;
+    pageNumber = 1;
+    error;
+
+    @track experiences;
+
     @wire(MessageContext) messageContext;
 
     @wire(getExperiences, { type: '$type', pageNumber: '$pageNumber' })
-    experiences;
+    wiredExperiences({ error, data }) {
+        if (data) {
+            this.pageSize = data.pageSize;
+            this.totalItemCount = data.totalItemCount;
+            this.experiences = data.records.map((experienceRecord) => {
+                const experience = { ...experienceRecord };
+                experience.isSelected = false;
+                return experience;
+            });
+        } else if (error) {
+            this.experiences = undefined;
+            this.error = error;
+        }
+    }
+
+    @wire(getPicklistValues, {
+        recordTypeId: '012000000000000AAA', // Default record type Id
+        fieldApiName: EXPERIENCE_TYPE_FIELD
+    })
+    getExperienceTypePicklistValues({ error, data }) {
+        if (data) {
+            this.types = data.values;
+            if (data.values.length > 0) {
+                this.type = data.values[0].value;
+            }
+        } else if (error) {
+            this.error = error;
+        }
+    }
 
     handleExperienceSelected(event) {
+        const experienceId = event.detail;
+        // Update selection
+        this.experiences.forEach((experience) => {
+            experience.isSelected = experience.Id === experienceId;
+        });
+        // Send Lightning message to notify selection
         publish(this.messageContext, EXPERIENCE_SELECTED_MESSAGE, {
-            experienceId: event.detail
+            experienceId
         });
     }
 
@@ -64,5 +76,9 @@ export default class ExperiencePanel extends LightningElement {
 
     handleNextPage() {
         this.pageNumber = this.pageNumber + 1;
+    }
+
+    get hasNoExperiences() {
+        return this.experiences && this.experiences.length === 0;
     }
 }
